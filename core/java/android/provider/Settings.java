e69b7228dc71fe5ab0e6febbad2e963c11eafec2
Darescu Ionut <ionut.darescu@gmail.com>
2014-12-21 14:51:36 +0800

    AdBlocker 2/3
    credit:tsubus

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3175,6 +3175,13 @@ public final class Settings {
          */
         public static final String VOICE_LAUNCH_INTENT = "voice_launch_intent";
 
+        
+        /**
+         * Disable ads (HFM)
+         * @hide
+         */
+        public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+        
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
