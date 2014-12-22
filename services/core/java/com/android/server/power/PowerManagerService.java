dc9dca8a3cba9ad63b0ecbf40ed3d66609035e3a
jackeagle <jackeagle102@gmail.com>
2014-12-22 05:46:50 -0500

    Revert "Temporary workaround"
    This reverts commit caf235638ceccbbe714f4b331a5be2d65f7d61c1.

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -739,10 +739,12 @@ public final class PowerManagerService extends SystemService
                 Settings.System.PROXIMITY_ON_WAKE, 0) == 1;
         
         mWakeLockBlockingEnabled = Settings.System.getIntForUser(resolver,
-                Settings.System.WAKELOCK_BLOCKING_ENABLED, 0) == 1;
+                Settings.System.WAKELOCK_BLOCKING_ENABLED,
+                0, UserHandle.USER_CURRENT);
 
         String blockedWakelockList = Settings.System.getStringForUser(resolver,
-                Settings.System.WAKELOCK_BLOCKING_LIST);
+                Settings.System.WAKELOCK_BLOCKING_LIST,
+                UserHandle.USER_CURRENT);
         setBlockedWakeLocks(blockedWakelockList);
 
         Slog.d(TAG, "mWakeLockBlockingEnabled=" + mWakeLockBlockingEnabled +
