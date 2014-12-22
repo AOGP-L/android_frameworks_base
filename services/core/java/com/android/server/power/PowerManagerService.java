c981c248c38f3ec640f449bed414f202f2a441f1
jackeagle <jackeagle102@gmail.com>
2014-12-22 05:47:33 -0500

    Revert "Error  parsing file issue"
    This reverts commit 5e409976a616e8c7578a18eab7e9657878f6deec.

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -3664,7 +3664,6 @@ public final class PowerManagerService extends SystemService
             String[] parts = wakeLockTagsString.split("\\|");
             for(int i = 0; i < parts.length; i++){
                 mBlockedWakeLocks.add(parts[i]);
-            }
         }
     }
 }
